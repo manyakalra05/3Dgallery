7e0a7ffacf102a89c13bf18a6d33bd5c708a0500
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2025-06-15 11:52:57 +0530

    Add files via upload

---
+++ src/utils/projectsData.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    id: 1,
+    title: "A Wormhole",
+    description: "Click on the link to travel through a Wormhole!",
+    link: "https://manyakalra05-wormhole.netlify.app/",
+    modelPath: "/models/wormhole.glb",
+    color: "#4287f5",
+    positionOffset: [0, 0.5, 0],
+    scale: 0.6
+  },
+  {
+    id: 2,
+    title: "Earth",
+    description: "Look at a model of our Earth!",
+    link: "https://manyakalra05-3dearth.netlify.app/",
+    modelPath: "/models/planet.glb",
+    color: "#f54242",
+    positionOffset: [0, -0.2, 0],
+    scale: 0.2
+  },
+  {
+    id: 3,
+    title: "Physics",
+    description: "Experience Physics like you've never before!",
+    link: "https://manyakalra05-physics.netlify.app/",
+    modelPath: "/models/physics.glb",
+    color: "#42f563",
+    positionOffset: [0, -1, 0],
+    scale: 1
+  }
+]
